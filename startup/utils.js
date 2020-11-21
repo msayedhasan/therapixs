0732ce8ce889ab4a9966df3c39d4d00458171b81
mhsayed278 <mhsayed278@gmail.com>
2020-11-21 12:11:01 +0200

    A

--- startup/utils.js
+++ startup/utils.js
@@ -7,18 +7,18 @@ const cors = require("cors");
 // // local image upload
 // const localUpload = require("./local-image-upload");
 // aws s3 image upload
-const awsUpload = require("./aws-s3-upload");
+// const awsUpload = require("./aws-s3-upload");
 
-module.exports = function (app) {
-  app.use(cors());
-  app.use(helmet());
-  app.use(compression());
-  app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(bodyParser.json());
+module.exports = function(app) {
+    app.use(cors());
+    app.use(helmet());
+    app.use(compression());
+    app.use(bodyParser.urlencoded({ extended: false }));
+    app.use(bodyParser.json());
 
-  // app.use(localUpload.single("image"));
-  // app.use(awsUpload.array("photos", 5));
-  // app.use(awsUpload.single("image"));
+    // app.use(localUpload.single("image"));
+    // app.use(awsUpload.array("photos", 5));
+    // app.use(awsUpload.single("image"));
 
-  app.use(express.json());
+    app.use(express.json());
 };
