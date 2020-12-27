334f714ce1c445675244bcc5b974b9009e277a43
mhsayed278 <mhsayed278@gmail.com>
2020-12-27 19:20:26 +0200

    A

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ require("./startup/utils")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 
-app.use(express.static(__dirname + '/dist/admin-panel'));
+app.use(express.static(__dirname + "/dist/admin-panel"));
 app.use("/images", express.static(path.join(__dirname, "images")));
 // Error handling function
 app.use(errors);
 
-process.on('uncaughtException', err => {
-  console.log(`Uncaught Exception: ${err.message}`)
-  process.exit(1)
-})
+process.on("uncaughtException", (err) => {
+  console.log(`Uncaught Exception: ${err.message}`);
+  process.exit(1);
+});
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("Unhandled rejection at ", promise, `reason: ${err.message}`);
+  process.exit(1);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log("Server connected");
+  console.log("Server connected");
 });
