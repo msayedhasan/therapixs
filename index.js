77380ce984eb245534e4ae211a6624bfa9e9e2b4
mhsayed278 <mhsayed278@gmail.com>
2020-12-30 21:05:55 +0200

    edit

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ require("./startup/utils")(app);
 require("./startup/routes")(app);
 require("./startup/db")();
 
-app.use(express.static(__dirname + "/dist/admin-panel"));
+// app.use(express.static(__dirname + "/dist/admin-panel"));
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("dist/admin-panel"));
+}
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "admin-panel", "index.html"));
+});
+
 app.use("/images", express.static(path.join(__dirname, "images")));
 // Error handling function
 app.use(errors);
