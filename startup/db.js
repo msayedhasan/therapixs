f9f987e1a417c99cdd8ccae8cef0678539a3fe8e
mhsayed278 <mhsayed278@gmail.com>
2021-10-15 20:12:24 +0200

    db

--- startup/db.js
+++ startup/db.js
@@ -3,8 +3,9 @@ const mongoose = require("mongoose");
 const winston = require("winston");
 const config = require("../config/index");
 
+const MONGODB_URI = 'mongodb+srv://msayed:msayed@motobar.i0mk5.mongodb.net/test';
 // const MONGODB_URI = 'mongodb://localhost:27017/motobike';
-const MONGODB_URI = 'mongodb://localhost:27017/MotoBar';
+// const MONGODB_URI = 'mongodb://localhost:27017/MotoBar';
 //const MONGODB_URI= "mongodb://root:isQT7E4LSVH4@100.25.187.101:27017/motobar?authSource=admin&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false"
 // "mongodb://root:haU8i2xjAZyV@54.226.145.221:27017/MotoBar?authSource=admin&readPreference=primary&appname=MongoDB%20Compass%20Community&ssl=false"
 
@@ -17,6 +18,7 @@ module.exports = () => {
             console.log("Connected to DB");
         })
         .catch((err) => {
+            console.log(err);
             winston.log(err);
         });
 };
