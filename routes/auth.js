58df8be0343db8461255971412fcd168369f1bcd
Mohamed Sayed <eng.msayedhasan@gmail.com>
2024-11-13 07:46:04 +0200

    a

--- routes/auth.js
+++ routes/auth.js
@@ -17,42 +17,11 @@ const router = express.Router();
 router.post("/signup", signupValidation, authController.signup);
 
 router.post("/login", authController.login);
-// router.post("/login", loginValidation, authController.login);
-
-// router.post("/resetPassword", authController.resetPassword);
 
 router.post("/verifyOtp", authController.verifyOtp);
 router.post("/sendOtp", authController.sendOtp);
 
-// router.post("/google", googleValidation, authController.google);
-// router.post(
-//   "/syncGoogle",
-//   passportJWT,
-//   googleValidation,
-//   authController.syncGoogle
-// );
-
-// router.post("/apple", authController.apple);
-// router.post("/syncApple", passportJWT, authController.syncApple);
-
-// router.post("/facebook", authController.facebook);
-// router.post("/syncFacebook", passportJWT, authController.syncFacebook);
-
 router.get("/me", authController.getProfile);
 
-// router.post(
-//   "/oauth/facebook",
-//   passport.authenticate("facebookToken", { session: false }),
-//   authController.facebookOAuth
-// );
-
-// router.post(
-//   "/oauth/google",
-//   // passport.authenticate(
-//   //     "google", { scope: ["email", "profile"] }, { session: false }
-//   // ),
-//   passport.authenticate("googleToken", { session: false }),
-//   authController.googleOAuth
-// );
 
 module.exports = router;
