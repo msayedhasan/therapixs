576b253cbf36f7709bb7b21dd1139ba56848ce2b
Mohamed Sayed <eng.msayedhasan@gmail.com>
2024-11-14 10:15:30 +0200

    a

--- index.js
+++ index.js
@@ -17,9 +17,9 @@ app.use(express.static(__dirname + "/dist"));
 // //set static folder
 // //  app.use(express.static("dist/admin-panel"));
 // //}
-// // app.get("*", (req, res) => {
-// //   res.sendFile(path.resolve(__dirname, "dist", "admin-panel", "index.html"));
-// // });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 app.use("/images", express.static(path.join(__dirname, "images")));
 // Error handling function
